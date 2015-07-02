54d99b4f2f898d3e778e8016bf677bd1441cc564
jaywhy13 <jeanmark.wright@gmail.com>
2015-07-02 09:36:36 -0500

    Start using dmetaphone instead of the older soundex

--- pgfuzzy/models.py
+++ pgfuzzy/models.py
@@ -9,7 +9,7 @@ class SoundsLike(Lookup):
         lhs, lhs_params = self.process_lhs(qn, connection)
         rhs, rhs_params = self.process_rhs(qn, connection)
         params = lhs_params + rhs_params
-        return 'soundex(%s) = soundex(%s)' % (lhs, rhs), params
+        return 'dmetaphone(%s) = dmetaphone(%s)' % (lhs, rhs), params
 
 class Soundex(Transform):
 
